13aeff28d3807359fd107c4df8aa81a99e7d88a4
wangyunlai.wyl <wangyunlai.wyl@antgroup.com>
2021-07-19 13:47:51 +0800

    fix(client): long-command support

--- src/obclient/client.cpp
+++ src/obclient/client.cpp
@@ -1,17 +1,12 @@
 /* Copyright (c) 2021 OceanBase and/or its affiliates. All rights reserved.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; version 2 of the License.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA */
+OceanBase is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details. */
 
 //
 // Created by Longda on 2021
@@ -27,7 +22,9 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/types.h>
+#include <sys/un.h>
 #include <unistd.h>
+#include <termios.h>
 
 #include "common/defs.h"
 #include "common/lang/string.h"
@@ -42,33 +39,40 @@ bool is_exit_command(const char *cmd) {
          0 == strncasecmp("bye", cmd, 3);
 }
 
-int main(int argc, char *argv[]) {
-  char *server_host = (char *)LOCAL_HOST;
-  if (argc >= 2) {
-    server_host = argv[1];
-  }
-  int server_port = PORT_DEFAULT;
-  if (argc >= 3) {
-    server_port = atoi(argv[2]);
+int init_unix_sock(const char *unix_sock_path) {
+  int sockfd = socket(PF_UNIX, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    fprintf(stderr, "failed to create unix socket. %s", strerror(errno));
+    return -1;
   }
 
-  const char *prompt_str = "miniob > ";
+  struct sockaddr_un sockaddr;
+  memset(&sockaddr, 0, sizeof(sockaddr));
+  sockaddr.sun_family = PF_UNIX;
+  snprintf(sockaddr.sun_path, sizeof(sockaddr.sun_path), "%s", unix_sock_path);
 
-  int sockfd, send_bytes;
-  // char send[MAXLINE];
+  if (connect(sockfd, (struct sockaddr *)&sockaddr, sizeof(sockaddr)) < 0) {
+    fprintf(stderr, "failed to connect to server. unix socket path '%s'. error %s",
+        sockaddr.sun_path, strerror(errno));
+    close(sockfd);
+    return -1;
+  }
+  return sockfd;
+}
 
-  char send_buf[MAX_MEM_BUFFER_SIZE];
-  // char buf[MAXDATASIZE];
+int init_tcp_sock(const char *server_host, int server_port) {
   struct hostent *host;
   struct sockaddr_in serv_addr;
 
   if ((host = gethostbyname(server_host)) == NULL) {
     fprintf(stderr, "gethostbyname failed. errmsg=%d:%s\n", errno, strerror(errno));
-    exit(1);
+    return -1;
   }
+
+  int sockfd;
   if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
     fprintf(stderr, "create socket error. errmsg=%d:%s\n", errno, strerror(errno));
-    exit(1);
+    return -1;
   }
 
   serv_addr.sin_family = AF_INET;
@@ -79,8 +83,72 @@ int main(int argc, char *argv[]) {
   if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr)) ==
       -1) {
     fprintf(stderr, "Failed to connect. errmsg=%d:%s\n", errno, strerror(errno));
-    exit(1);
+    close(sockfd);
+    return -1;
   }
+  return sockfd;
+}
+
+int set_terminal_noncanonical() {
+  int fd = STDIN_FILENO;
+  struct termios old_termios;
+  int ret = tcgetattr(fd, &old_termios);
+  if (ret < 0) {
+    printf("Failed to get tc attr. error=%s\n", strerror(errno));
+    return -1;
+  }
+
+  struct termios new_attr = old_termios;
+  new_attr.c_lflag &= ~ICANON;
+  ret = tcsetattr(fd, TCSANOW, &new_attr);
+  if (ret < 0) {
+    printf("Failed to set tc attr. error=%s\n", strerror(errno));
+    return -1;
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  int ret = set_terminal_noncanonical();
+  if (ret < 0) {
+    printf("Warning: failed to set terminal non canonical. Long command may be handled incorrect\n");
+  }
+
+  const char *unix_socket_path = nullptr;
+  const char *server_host = "127.0.0.1";
+  int server_port = PORT_DEFAULT;
+  int opt;
+  extern char *optarg;
+  while ((opt = getopt(argc, argv, "s:h:p:")) > 0) {
+    switch (opt) {
+    case 's':
+      unix_socket_path = optarg;
+      break;
+    case 'p':
+      server_port = atoi(optarg);
+      break;
+    case 'h':
+      server_host = optarg;
+      break;
+    }
+  }
+
+  const char *prompt_str = "miniob > ";
+
+  int sockfd, send_bytes;
+  // char send[MAXLINE];
+
+  if (unix_socket_path != nullptr) {
+    sockfd = init_unix_sock(unix_socket_path);
+  } else {
+    sockfd = init_tcp_sock(server_host, server_port);
+  }
+  if (sockfd < 0) {
+    return 1;
+  }
+
+  char send_buf[MAX_MEM_BUFFER_SIZE];
+  // char buf[MAXDATASIZE];
 
   fputs(prompt_str, stdout);
   while (fgets(send_buf, MAX_MEM_BUFFER_SIZE, stdin) != NULL) {
@@ -93,7 +161,7 @@ int main(int argc, char *argv[]) {
       break;
     }
 
-    if ((send_bytes = send(sockfd, send_buf, strlen(send_buf) + 1, 0)) == -1) {
+    if ((send_bytes = write(sockfd, send_buf, strlen(send_buf) + 1)) == -1) {
       fprintf(stderr, "send error: %d:%s \n", errno, strerror(errno));
       exit(1);
     }
